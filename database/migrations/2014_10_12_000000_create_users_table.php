ebd093b0206d47e4dbd11e6444fc2129b4795856
Eduardo Oliveira <eduardomoliveira@apoioprodesp.sp.gov.br>
2020-09-28 18:06:06 -0300

    Ativando o sistema de login do Laravel 8 - Auth.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum('admin',['S','N'])->default('N');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
