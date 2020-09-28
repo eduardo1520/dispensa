32501d2aa60ed8d69d41f94dfd15cdd8f9c3ac4b
Eduardo Oliveira <eduardomoliveira@apoioprodesp.sp.gov.br>
2020-09-28 17:51:58 -0300

    Configurando o banco de dados SQLite e rodando a migrate de usuário.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('admin',['S','N'])->default('N');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
